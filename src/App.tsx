e3a587e487236951477e713b1db35c68aa03b4b0
gkshtz <gkshitiz21@gmail.com>
2024-10-23 23:12:47 +0530

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './Components/LoginComponent/Login'
+import Dashboard from './Components/DashboardComponent/Dashboard'
 
 function App() {
   const router = createBrowserRouter([
     {
+      path: '/',
+      element: <Login></Login>
+    },
+    {
       path: '/login',
       element: <Login/>
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard/>
     }
   ])
   
